4541bd52c2301ab86db9761d52442f2c2e6fdfd4
gulfamsaifi931 <147502040+gulfamsaifi931@users.noreply.github.com>
2023-10-30 12:31:40 +0530

    sdfdsf

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,38 @@
-import TopicsList from "@/app/list/page";
+import Link from "next/link";
 
-export default function Home() {
-  return <TopicsList />;
+
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+};
+
+export default async function TopicsList() {
+  const { topics } = await getTopics();
+
+  return (
+    <>
+      {topics && topics.map((t) => (
+        <div
+          key={t._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <div>{t.description}</div>
+          </div>
+
+        </div>
+      ))}
+    </>
+  );
 }
